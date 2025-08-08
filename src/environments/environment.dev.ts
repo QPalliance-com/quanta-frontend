0b4e05d8b79d54fa97c775c7eb641dbf762bc1c0
jleonqpalliance <jleon@qpalliance.co>
2025-08-08 08:48:31 -0500

    suspend deploy, add enviroment localhost

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
     apiUrl: 'http://localhost:8000/api/v1/',
-    environmentName: 'development'
+    environmentName: 'dev'
     // Add any other environment-specific variables here
 };
